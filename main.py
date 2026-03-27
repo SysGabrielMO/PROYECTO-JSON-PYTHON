15b619cb6a89db81bfa304ee727ac7f75cd9137c
Gabriel <gabrielbusiness2007@gmail.com>
2026-03-27 12:11:32 +0100

    Archivo main creado con menú

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+MENU = """
+          EUROCOPA 2024 - MENU PRINCIPAL
+-------------------------------------------------
+  1. Listar todos los partidos                
+  2. Tarjetas amarillas (total y por equipo)  
+  3. Buscar partidos de un equipo             
+  4. Buscar goles de un jugador               
+  5. Clasificacion por goles                  
+  0. Salir                                    
+
+"""
+print(MENU)
+
